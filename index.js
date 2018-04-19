862b933ba8c81697e19b918a2adfa5ec3e8147e0
Laurents Mohr <laurents.mohr@whu.edu>
2018-04-19 21:09:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -173,12 +173,12 @@ function positionToInteger(p) {
 };
 
 function start() {
-  window.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
   }, 1000);
-  return gameInterval;
+  
 };
